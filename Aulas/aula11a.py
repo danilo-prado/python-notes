4afea6383a458e5f1595d642b0e2ff07901f8625
danilo-prado <83561424+danilo-prado@users.noreply.github.com>
2021-12-03 20:01:10 -0300

    Delete Aulas directory

--- Aulas/aula11a.py
+++
@@ -1,5 +0,0 @@
-'''SISTEMA DE CORES'''
-
-'''print('\033[0:37:40mDanilão é o campeão!!!\033[m')'''
-
-print(3 * 5 + 4 ** 2)
--- Aulas/aula12a.py
+++
@@ -1,7 +0,0 @@
-nome = str(input('Qual é seu nome?'))
-if nome == 'Danilo':
-    print('Que nome bonito!')
-elif nome == 'João' or nome == 'Claudia' or nome == 'Patrícia':
-    print('Uia só que legal hein')
-else:
-    print('Que nome de danadinho')

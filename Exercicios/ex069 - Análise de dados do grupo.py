e48a2b40d25cd62681fdecb8b11eaa8b94e8b923
danilo-prado <83561424+danilo-prado@users.noreply.github.com>
2021-12-04 17:26:26 -0300

    Commit

--- Exercicios/ex069 - Análise de dados do grupo.py
+++ Exercicios/ex069 - Análise de dados do grupo.py
@@ -2,4 +2,23 @@
 se o usuário quer ou não continuar. No final, mostre:
 A) Quantas pessoas tem mais de 18 anos
 B) Quantos homens foram cadastrados
-C) Qauntas mulheres tem menos de 20 anos'''
+C) Quantas mulheres tem menos de 20 anos'''
+
+cont = men = women = 0
+
+while True:
+    age = int(input('Digite a idade: '))
+    if age > 18:
+        cont += 1
+    gender = str(input('Digite o sexo: [M/F]')).upper().strip()[0]
+    if gender == 'M':
+        men += 1
+    print('-'*10)
+    ask = str(input('Deseja continuar? [S/N]')).upper().strip()[0]
+    if gender == 'F' and age < 20:
+        women += 1
+    if ask == 'N':
+        print(f'Há {cont} pessoa(s) maior(es) de 18 anos')
+        print(f'Há {men} homen(s) cadastrado(s)')
+        print(f'Há {women} mulhere(s) com menos de 20 anos')
+        break

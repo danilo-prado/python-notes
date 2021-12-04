0dac5f0f619ffb5c7fe670b6b337a9a89f1d755d
danilo-prado <83561424+danilo-prado@users.noreply.github.com>
2021-12-04 18:42:27 -0300

    Commit

--- Exercicios/ex071 - Simulador de Caixa Eletrônico.py
+++ Exercicios/ex071 - Simulador de Caixa Eletrônico.py
@@ -1,3 +1,34 @@
-'''Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor
+'''Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual
+será o valor
 a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
 Obs: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1'''
+
+
+print('='*30)
+print('{:^30}'.format('BANCO XPTO'))
+print('='*30)
+
+n = int(input('Digite o valor: R$'))
+total = n
+ced = 50
+totced = 0
+
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('='*30)
+print('{:^30}'.format('VOLTE SEMPRE!'))
+print('='*30)

5a8c006a2458b9fa838ed1edad4f84f9879b763b
danilo-prado <83561424+danilo-prado@users.noreply.github.com>
2021-12-04 16:48:01 -0300

    Resolução ex068

--- Exercicios/ex068 - Jogo do Par ou Ímpar.py
+++ Exercicios/ex068 - Jogo do Par ou Ímpar.py
@@ -1,2 +1,35 @@
-'''Faça um programa que ogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador PERDER,
+'''Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador PERDER,
 mostrando o total de vitórias consecutivas que ele conquistou no final do jogo'''
+
+from random import randint
+
+win = 0
+lose = 0
+
+while True:
+    #choice = input('PAR OU ÍMPAR? [P/I]:')
+    player = int(input('Digite o número: '))
+    cpu = randint(0, 10)
+    soma = player + cpu
+    choice = input('PAR OU ÍMPAR? [P/I] ').upper().strip()[0]
+
+    if choice != 'P' and choice != 'I':
+        print('Opção inválida... Tente novamente!')
+
+
+    elif choice == 'P' and soma % 2 == 0:
+        print(f'CPU: {cpu}')
+        print('VOCÊ GANHOU :)')
+        win += 1
+
+
+    elif choice == 'I' and soma % 2 != 0:
+        print(f'CPU: {cpu}')
+        print('VOCÊ GANHOU :)')
+        win += 1
+    else:
+        print(f'CPU: {cpu}')
+        print('VOCÊ PERDEU :(')
+        lose += 1
+        print(f'Você jogou {win + lose} vezes e ganhou {win} vezes')
+        break
